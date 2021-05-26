32e9ccc0486de959d8fbe7c91478e2ad9b6ad34d
Roman Rädle <raedle@fb.com>
2021-05-25 20:51:40 -0700

    Initial PyTorch Live Docusaurus config
    Summary: Initial Docusaurus configuration for PyTorch Live. The change also adds the FB OSS logo.
    
    Reviewed By: simonpfish
    
    Differential Revision: D28596134
    
    fbshipit-source-id: e9e0f1e9b113f93a212c1ffd45ccecbfe39f2d7e

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -3,24 +3,26 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @format
  */
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'PyTorch Live',
+  tagline: 'Build mobile AI experiences in just a few minutes',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'facebookexperimental',
+  projectName: 'pytorch-live',
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'PyTorch Live',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'PyTorch Live Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -32,7 +34,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/facebookexperimental/pytorch-live',
           label: 'GitHub',
           position: 'right',
         },
@@ -55,15 +57,7 @@ module.exports = {
           items: [
             {
               label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://stackoverflow.com/questions/tagged/pytorch-live',
             },
           ],
         },
@@ -80,8 +74,32 @@ module.exports = {
             },
           ],
         },
+        {
+          title: 'Legal',
+          // Please do not remove the privacy and terms, it's a legal requirement.
+          items: [
+            {
+              label: 'Privacy',
+              href: 'https://opensource.facebook.com/legal/privacy/',
+              target: '_blank',
+              rel: 'noreferrer noopener',
+            },
+            {
+              label: 'Terms',
+              href: 'https://opensource.facebook.com/legal/terms/',
+              target: '_blank',
+              rel: 'noreferrer noopener',
+            },
+          ],
+        },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      logo: {
+        alt: 'Facebook Open Source Logo',
+        src: 'img/oss_logo.png',
+        href: 'https://opensource.facebook.com',
+      },
+      // Please do not remove the credits, help to publicize Docusaurus :)
+      copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -90,15 +108,15 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/facebookexperimental/pytorch-live/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/facebookexperimental/pytorch-live/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

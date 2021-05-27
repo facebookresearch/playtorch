73bfce8d3ceb09cadabb194db599f462d21b8340
Roman Rädle <raedle@fb.com>
2021-05-27 09:27:59 -0700

    Camera component doc
    Summary: Typedoc `Camera` component.
    
    Reviewed By: simonpfish
    
    Differential Revision: D28701824
    
    fbshipit-source-id: 6e5de80d5caf1412c10390e54462f242ed3cb758

--- react-native-pytorch-core/src/CameraView.tsx
+++ react-native-pytorch-core/src/CameraView.tsx
@@ -13,38 +13,112 @@ import {requireNativeComponent, ViewProps} from 'react-native';
 import {Image, wrapRef} from './ImageModule';
 import type {NativeJSRef} from './NativeJSRef';
 
-type CameraProps = {
-  hideCaptureButton?: boolean,
-  onCapture?(image: Image): void,
-  onFrame?(image: Image): void,
-} & ViewProps;
+/**
+ * Properties for the camera.
+ *
+ * ```typescript
+ * <Camera
+ *   onFrame={(image: Image) => {
+ *     image.release();
+ *   }}
+ *   hideCaptureButton={true}
+ * />
+ * ```
+ */
+export interface CameraProps extends ViewProps {
+  /**
+   * Hides the capture button if set to `true`, otherwise the camera will show
+   * a capture button.
+   */
+  hideCaptureButton?: boolean;
+
+  /**
+   * Callback with an [[Image]] after capture button was pressed.
+   *
+   * @param image An [[Image]] reference.
+   */
+  onCapture?(image: Image): void;
+
+  /**
+   * Callback when the camera delivers an [[Image]].
+   *
+   * :::caution
+   *
+   * Needs to call [[Image.release]] to receive the next frame. The camera
+   * preview will continue to render updates, but new [[Image]] frames will be
+   * omitted until [[Image.release]] is called.
+   *
+   * :::
+   *
+   * @param image An [[Image]] reference.
+   */
+  onFrame?(image: Image): void;
+}
 
 const PyTorchCoreCameraView = requireNativeComponent<CameraProps>(
-  'PyTorchCoreCameraView',
+  'PyTorchCoreCameraView'
 );
 
-export function Camera({onFrame, onCapture, hideCaptureButton, ...otherProps}: CameraProps) {
+/**
+ * A camera component with [[CameraProps.onCapture]] and [[CameraProps.onFrame]] callbacks.
+ *
+ * ```typescript
+ * export default function App() {
+ *   const {imageClass, processImage} = useImageClassification(
+ *     require('./resnet18.pt'),
+ *   );
+ *
+ *   const handleFrame = useCallback(
+ *     async (image: Image) => {
+ *       await processImage(image);
+ *       image.release();
+ *     },
+ *     [processImage],
+ *   );
+ *
+ *   return (
+ *     <>
+ *       <Camera
+ *         style={styles.camera}
+ *         onFrame={handleFrame}
+ *         hideCaptureButton={true}
+ *       />
+ *       <Text>{imageClass}</Text>
+ *     </>
+ *   );
+ * }
+ * ```
+ *
+ * @component
+ */
+export function Camera({
+  onFrame,
+  onCapture,
+  hideCaptureButton,
+  ...otherProps
+}: CameraProps) {
   const handleFrame = useCallback(
     (event: any) => {
-      const {nativeEvent} = event;
-      const {ID} = nativeEvent;
-      const ref: NativeJSRef = {ID};
+      const { nativeEvent } = event;
+      const { ID } = nativeEvent;
+      const ref: NativeJSRef = { ID };
       const image = wrapRef(ref);
       onFrame != null && onFrame(image);
     },
-    [onFrame],
+    [onFrame]
   );
 
   const handleCapture = useCallback(
     (event: any) => {
-      const {nativeEvent} = event;
-      const {ID} = nativeEvent;
-      const ref: NativeJSRef = {ID};
+      const { nativeEvent } = event;
+      const { ID } = nativeEvent;
+      const ref: NativeJSRef = { ID };
       const image = wrapRef(ref);
       onCapture != null && onCapture(image);
     },
-    [onCapture],
+    [onCapture]
   );
+
   return (
     <PyTorchCoreCameraView
       {...otherProps}

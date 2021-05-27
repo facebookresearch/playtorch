b590e20376b7ebbef2da2bd3876ee367c92b1628
Roman Rädle <raedle@fb.com>
2021-05-27 09:27:59 -0700

    MobileModel module doc
    Summary: Typedoc for `MobileModel` module.
    
    Reviewed By: simonpfish
    
    Differential Revision: D28701825
    
    fbshipit-source-id: fe1d8676652518a0630b412255f2c01d6e8f9e93

--- react-native-pytorch-core/src/MobileModelModule.ts
+++ react-native-pytorch-core/src/MobileModelModule.ts
@@ -18,8 +18,18 @@ const {resolveAssetSource} = Image;
 
 const {PyTorchCoreMobileModelModule: MobileModelModule} = NativeModules;
 
+/**
+ * Cache for previously resolved model paths (i.e., asset sources).
+ */
 const MODEL_PATH_CACHE: {[key: number]: ImageResolvedAssetSource} = {};
 
+/**
+ * Resolves the model asset source. It will first try to find the asset source
+ * in the cache if it was previously resolved, otherwise it will use the
+ * `resolveAssetSource` provided by the React Native [[Image]].
+ *
+ * @param modelPath The model path (i.e., a `require`).
+ */
 const getModelAssetSource = (
   modelPath: ImageRequireSource,
 ): ImageResolvedAssetSource => {
@@ -31,14 +41,84 @@ const getModelAssetSource = (
   return source;
 };
 
-export const MobileModel = {
-  getModelAssetSource,
+/**
+ * Result of model inference. Each model result has the inference time and the
+ * model result. The model result depends on the model and is therefore
+ * specified as a generic type (i.e., template).
+ *
+ * @template T Model result type
+ */
+export interface ModelResult<T> {
+  /**
+   * The model result.
+   */
+  result: T;
+
+  /**
+   * The model inference time in milliseconds.
+   */
+  inferenceTime: number;
+}
+
+/**
+ * @packageDocumentation
+ *
+ * The `MobileModel` is the core module providing functions to run model
+ * inference and preload models.
+ */
+export interface MobileModel {
+  /**
+   * Preload a model. If a model is not preloaded, it will be loaded during the
+   * first inference call. However, the first inference time will therefore
+   * take significantly longer. This function allows to preload a model ahead
+   * of time before running the first inference.
+   *
+   * @param modelPath The model path (i.e., a `require`).
+   */
+  preload(modelPath: ImageRequireSource): Promise<void>;
+
+  /**
+   * Run inference on a model.
+   *
+   * ```typescript
+   * const classificationModel = require('../models/mobilenet_v3_small.pt');
+   *
+   * const image: Image = await ImageUtils.fromURL('https://image.url');
+   *
+   * const { result: {scores} } = await MobileModel.execute(
+   *   classificationModel,
+   *   {
+   *     image,
+   *     cropWidth: size,
+   *     cropHeight: size,
+   *     scaleWidth: 224,
+   *     scaleHeight: 224,
+   *   }
+   * );
+   *
+   * const idx = argmax(scores);
+   * const className = CLASSES[idx];
+   * ```
+   *
+   * @param modelPath The model path (i.e., `require`).
+   * @param params The input parameters for the model.
+   */
+  execute<T>(
+    modelPath: ImageRequireSource,
+    params: any,
+  ): Promise<ModelResult<T>>;
+}
+
+export const MobileModel: MobileModel = {
   async preload(modelPath: ImageRequireSource): Promise<void> {
     const source = getModelAssetSource(modelPath);
     return await MobileModelModule.preload(source.uri);
   },
-  async execute(modelPath: ImageRequireSource, data: any): Promise<any> {
+  async execute<T>(
+    modelPath: ImageRequireSource,
+    params: any,
+  ): Promise<ModelResult<T>> {
     const source = getModelAssetSource(modelPath);
-    return await MobileModelModule.execute(source.uri, data);
+    return await MobileModelModule.execute(source.uri, params);
   },
 };

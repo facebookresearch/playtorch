4475f05d77807012537f12ea9bdf1ce97cfae834
Roman Rädle <raedle@fb.com>
2021-06-01 22:31:20 -0700

    Preload models on Android-only
    Summary:
    Preload models on Android-only until we have implemented the same PyTorch Mobile module on iOS. This will prevent the app from showing an error instantly on bundle load.
    
    This change can be deprecated once we have PyTorch Mobile module on iOS.
    
    Reviewed By: castillejoale
    
    Differential Revision: D28817684
    
    fbshipit-source-id: 9728e6c7ec6588b3bd1a5c056d7e46e3e913da41

--- react-native-template-pytorch-live/template/src/App.tsx
+++ react-native-template-pytorch-live/template/src/App.tsx
@@ -15,49 +15,84 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Examples from './examples/Examples';
 import Demos from './demos/MyDemos';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import ModelPreloader from './components/ModelPreloader';
 import {Models} from './Models';
 import Toolbox from './toolbox/Toolbox';
 
+Icon.loadFont();
+
 // Before rendering any navigation stack
 import {enableScreens} from 'react-native-screens';
+import {Platform, SafeAreaView, StyleSheet} from 'react-native';
+import {ReactNode} from 'react';
 enableScreens();
 
 const Tab = createBottomTabNavigator();
 
+type ModelPreloaderWrapperProps = {
+  children?: ReactNode | ReactNode[];
+};
+
+/**
+ * Wrapper around the `ModelPreloader` until PyTorch Live supports iOS as a
+ * platform.
+ */
+function ModelPreloaderWrapper({children}: ModelPreloaderWrapperProps) {
+  if (Platform.OS !== 'android') {
+    <ModelPreloader modelInfos={Models} loadAsync={true}>
+      {children}
+    </ModelPreloader>;
+  }
+  return <>{children}</>;
+}
+
+function AppWithNavigation() {
+  const insets = useSafeAreaInsets();
+  return (
+    <ModelPreloaderWrapper>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Examples"
+          screenOptions={({route}) => ({
+            tabBarIcon: ({color, size}) => {
+              if (route.name === 'Toolbox') {
+                return <Icon name="drawing-box" size={size} color={color} />;
+              } else if (route.name === 'Examples') {
+                return <Icon name="fire" size={size} color={color} />;
+              } else if (route.name === 'My Demos') {
+                return <Icon name="star" size={size} color={color} />;
+              }
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#f60',
+            inactiveTintColor: '#fff',
+            style: {
+              backgroundColor: '#000',
+              paddingTop: 10,
+              height: 60 + insets.bottom,
+            },
+            labelStyle: {
+              height: 22,
+            },
+          }}>
+          <Tab.Screen name="My Demos" component={Demos} />
+          <Tab.Screen name="Examples" component={Examples} />
+          <Tab.Screen name="Toolbox" component={Toolbox} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ModelPreloaderWrapper>
+  );
+}
+
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ModelPreloader modelInfos={Models} loadAsync={true}>
-        <NavigationContainer>
-          <Tab.Navigator
-            initialRouteName="Examples"
-            screenOptions={({route}) => ({
-              tabBarIcon: ({color, size}) => {
-                if (route.name === 'Toolbox') {
-                  return <Icon name="drawing-box" size={size} color={color} />;
-                } else if (route.name === 'Examples') {
-                  return <Icon name="fire" size={size} color={color} />;
-                } else if (route.name === 'My Demos') {
-                  return <Icon name="star" size={size} color={color} />;
-                }
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: '#f60',
-              inactiveTintColor: '#fff',
-              labelStyle: {
-                height: 22,
-              },
-              style: {backgroundColor: '#000', height: 60, paddingTop: 10},
-            }}>
-            <Tab.Screen name="My Demos" component={Demos} />
-            <Tab.Screen name="Examples" component={Examples} />
-            <Tab.Screen name="Toolbox" component={Toolbox} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </ModelPreloader>
+      <AppWithNavigation />
     </SafeAreaProvider>
   );
 }

46f7a681d02067c2fcb4542770dd237f505c7fe9
Clark Sandholtz <clarksandholtz@fb.com>
2021-06-17 15:41:37 -0700

    Adding survey links to the bottom of each tutorial
    Summary: Adding a small section at the bottom of each tutorial that links to a survey for tutorial evaluation.
    
    Reviewed By: raedle
    
    Differential Revision: D29120636
    
    fbshipit-source-id: 4da80cec4fde9d9ed6a27617b401cf80e941c663

---
+++ website/src/components/SurveyLinkButton.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import styles from './SurveyLinkButton.module.css';
+
+export default function SurveyLinkButton({docTitle}) {
+  const surveyUrl = `https://docs.google.com/forms/d/e/1FAIpQLScsB21xJWM_VANad5GcVkQqKB_BptS77axbunzs7ZkwoE5JUw/viewform?usp=pp_url&entry.1880917601=${docTitle.replace(/\s/g, '+')}`
+  return (
+    <a href={surveyUrl} target="_blank">
+      <div className={styles.surveyLinkBox}>
+        Share what we can improve!
+      </div>
+    </a>
+  );
+}

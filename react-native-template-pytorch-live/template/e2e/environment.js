9938cfb7c270c8fb5a620a39aa61b36bad0365ac
Clark Sandholtz <clarksandholtz@fb.com>
2021-06-24 09:39:46 -0700

    Initial Detox setup
    Summary:
    Adding detox and simple example test for e2e testing of PyTorch Live template app
    allow-large-files
    
    Reviewed By: raedle
    
    Differential Revision: D29221226
    
    fbshipit-source-id: e7ac811d4977ef57536abffe303018fd4ea59e95

---
+++ react-native-template-pytorch-live/template/e2e/environment.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const {
+  DetoxCircusEnvironment,
+  SpecReporter,
+  WorkerAssignReporter,
+} = require('detox/runners/jest-circus');
+
+class CustomDetoxEnvironment extends DetoxCircusEnvironment {
+  constructor(config, context) {
+    super(config, context);
+
+    // Can be safely removed, if you are content with the default value (=300000ms)
+    this.initTimeout = 300000;
+
+    // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
+    // This is strictly optional.
+    this.registerListeners({
+      SpecReporter,
+      WorkerAssignReporter,
+    });
+  }
+}
+
+module.exports = CustomDetoxEnvironment;

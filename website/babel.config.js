0d95ec575d89202b7e59cc24d44b407547372c0f
Roman Rädle <raedle@fb.com>
2021-09-21 11:25:06 -0700

    Change test regex for external fb/ import stripping
    Summary:
    The external OSS website build strips `fb/` imports on build, which allows us to add internal-only content. Docusaurus supports absolute paths via the `site/` prefix. However, this wasn't considered in the initial test for the `babel-plugin-transform-remove-imports`.
    
    John Know suggested changing the regex to also catch absolute paths and strip them as well for the OSS build of the website
    
    https://fb.workplace.com/groups/docusaurus/posts/1437442033294580?comment_id=1438213593217424&reply_comment_id=1438934826478634
    
    Reviewed By: clarksandholtz
    
    Differential Revision: D31083596
    
    fbshipit-source-id: dc6971cb40dbef4519e8069fd09e56cb4d948cb8

--- website/babel.config.js
+++ website/babel.config.js
@@ -11,14 +11,14 @@ module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
   // This is needed to strip fb/ imports in pure React components
   // during the OSS build.
-  // TODO() Evaluate if stripping fb/ imports is still needed
+  // TODO(T101250738) Evaluate if stripping fb/ imports is still needed
   plugins: process.env.FB_INTERNAL
     ? []
     : [
         [
           'babel-plugin-transform-remove-imports',
           {
-            test: '.+/(.*/)?fb/',
+            test: '^(\@site|\.+)\/(.*\/)?fb\/',
           },
         ],
       ],

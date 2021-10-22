f75af9bed02086436985732dbd2e117cb50441fd
Clark Sandholtz <clarksandholtz@fb.com>
2021-10-22 09:45:15 -0700

    Reference files for Question Answering tutorial
    Summary: Just adding the final state of the files created and modified in the Question Answering tutorial to the examples folder
    
    Reviewed By: raedle
    
    Differential Revision: D31221717
    
    fbshipit-source-id: be9cd37326ff34f4258a679376460097f73ea097

---
+++ examples/nlp-tutorials/QuestionAnsweringTutorial/MyDemos.tsx
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import QuestionAnsweringDemo from './QuestionAnsweringDemo';
+
+export default function MyDemos() {
+  return (
+    <QuestionAnsweringDemo />
+  );
+}

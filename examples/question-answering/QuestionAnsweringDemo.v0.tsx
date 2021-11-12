50c35667ac50ecb887afba931287b450d49f9201
Clark Sandholtz <clarksandholtz@fb.com>
2021-11-12 10:03:23 -0800

    Reference files for Question Answering tutorial
    Summary: Adds reference files to Question Answering tutorial and fixes imports of `ModelInfo`
    
    Reviewed By: raedle
    
    Differential Revision: D32375307
    
    fbshipit-source-id: dc9351d350150d0d3a2fd94a27dca539a5309f45

---
+++ examples/question-answering/QuestionAnsweringDemo.v0.tsx
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+
+export default function QuestionAnsweringDemo() {
+    return (
+        <View>
+            <TextInput placeholder="Text" />
+            <TextInput placeholder="Question" />
+            <Button title="Ask" onPress={() => { }} />
+            <Text>Question Answering</Text>
+        </View>
+    );
+}
---
+++ examples/question-answering/QuestionAnsweringDemo.v4.tsx
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import type { ModelInfo } from 'react-native-pytorch-core';
+import useNLPQAModelInference from '../useNLPQAModelInference';
+
+const modelInfo: ModelInfo = {
+    name: 'Bert Q&A',
+    model: require('../../models/bert_qa.ptl'),
+}
+
+export default function QuestionAnsweringDemo() {
+    const [text, setText] = useState('');
+    const [question, setQuestion] = useState('');
+    const {
+        answer,
+        isProcessing,
+        processQA,
+    } = useNLPQAModelInference(modelInfo);
+
+    function handleAsk() {
+        processQA(text, question);
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={[styles.item, styles.input]} placeholder="Text" placeholderTextColor="#CCC" multiline={true} value={text} onChangeText={setText} />
+            <TextInput style={[styles.item, styles.input]} placeholder="Question" placeholderTextColor="#CCC" value={question} onChangeText={setQuestion} />
+            <Button title="Ask" onPress={handleAsk} />
+            <Text style={styles.item}>{isProcessing ? "Looking for the answer" : answer}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    item: {
+        margin: 10,
+    },
+    input: {
+        borderWidth: 1,
+        color: '#000',
+    }
+});

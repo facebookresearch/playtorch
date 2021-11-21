9237864396ba207c1afdb819ad993d07f8348706
Yinglao Liu <yinglao@fb.com>
2021-11-21 02:15:15 -0800

    log error of post-init scripts
    Summary:
    The post-init scripts is executed behind the scene and users won't have any ideas if the script success or not.
    
    Here we explicitly log the error to file for future retrieval in the init command
    
    Reviewed By: raedle
    
    Differential Revision: D32574468
    
    fbshipit-source-id: a9eacc6a8d410b338020e554fa254522f00517f3

--- react-native-template-pytorch-live/script.js
+++ react-native-template-pytorch-live/script.js
@@ -14,12 +14,12 @@ switch(process.platform) {
     case 'darwin':
     case 'linux':
         execSync(
-            'cd ./models && python3 -m venv ./venv \
-            && source ./venv/bin/activate \
-            && pip install --upgrade pip \
-            && pip install -r requirements.txt \
-            && python make_models.py'
-        )
+            'cd ./models && python3 -m venv ./venv 2> ./error.log \
+            && source ./venv/bin/activate 2> ./error.log \
+            && pip install --upgrade pip 2> ./error.log \
+            && pip install -r requirements.txt 2> ./error.log \
+            && python make_models.py 2> ./error.log'
+        );
         break;
     case 'win32':
         // TODO: Implement

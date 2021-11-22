8f7757d00702353c1847c25ae8f519e13f1cca27
William Ngan <wnn@fb.com>
2021-11-22 10:15:43 -0800

    Design updates in Community page
    Summary: Implementing James' new design. Contents are placeholders only.
    
    Reviewed By: raedle
    
    Differential Revision: D32577539
    
    fbshipit-source-id: 71c32a24d2a3ba6174860c8bfb11433cb0fda8ef

--- website/src/pages/community.js
+++ website/src/pages/community.js
@@ -12,89 +12,97 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import styles from './community.module.css';
 
+function Card({thumbnail, title, children}) {
+  const imgUrl = thumbnail
+    ? require(`@site/static/img/${thumbnail}`).default
+    : require(`@site/static/img/default_card.jpg`).default;
+  return (
+    <div className={styles.showCard}>
+      <div
+        className={styles.cardThumbnail}
+        style={{backgroundImage: `url(${imgUrl})`}}></div>
+      <div className={styles.cardContent}>
+        <h4 className={styles.cardTitle}>{title}</h4>
+        <p>{children}</p>
+      </div>
+    </div>
+  );
+}
+
+function SocialIcon({name, color = 'white'}) {
+  const iconUrl = require(`@site/static/img/icon_${name}_${color}.png`).default;
+  return <img className={styles.socialIcon} src={iconUrl} />;
+}
+
 function Community() {
+  const graphicUrl = require('@site/static/img/community_network.png').default;
+
   return (
     <Layout title="Community">
       <div className={clsx('hero--primary', styles.heroBanner)}>
-        <h1 className={clsx('hero__title', styles.heroTitle)}>
-          Let’s help each other and democratize AI together. Join our community.
-        </h1>
-        <div>
-          <div className={styles.socialCard}>Join us on Discord</div>
-          <div className={styles.socialCard}>Collaborate on GitHub</div>
-          <div className={styles.socialCard}>Tag #pytorchlive on Twitter</div>
+        <div className="container">
+          <div className="row">
+            <div className="col col--6">
+              <h1 className={styles.title}>Community</h1>
+              <div className={styles.subtitle}>
+                Let's unlock the vast potential of AI innovations together
+              </div>
+              <div className={styles.social}>
+                <SocialIcon name="discord" />
+                <SocialIcon name="github" />
+                <SocialIcon name="twitter" />
+              </div>
+            </div>
+            <div className="col col--6">
+              <div
+                className={styles.graphic}
+                style={{backgroundImage: `url(${graphicUrl})`}}></div>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.showcase}>
-        <div className={clsx(['container', 'margin-bottom--xl'])}>
+        <div className={clsx(['container', 'margin-bottom--l'])}>
           <div className="row">
-            <div className="col col--9">
-              <h2>PyTorch Live Showcase</h2>
+            <div className="col col--12">
+              <h2>Community Showcase</h2>
               <p className={styles.showcaseInfo}>
                 Share what you have made with us. Create a PR in GitHub or fill
                 out <a href="#">this form</a>. <br />
                 Here is a selection of PyTorch Live demos created by us and the
                 community.
               </p>
-
-              <div className="row">
-                <div className="col col--6">
-                  <div className={styles.showCard}>
-                    <div className={styles.cardThumbnail}></div>
-                    <div className={styles.cardContent}>
-                      <h4>Mario and Luigi</h4>
-                      <p>
-                        Roman finetuned the MobileNetV3 model to identify
-                        Nintendo characters.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col col--6">
-                  <div className={styles.showCard}>
-                    <div className={styles.cardThumbnail}></div>
-                    <div className={styles.cardContent}>
-                      <h4>Peach</h4>
-                      <p>
-                        William finetuned the MobileNetV3 model to identify
-                        fruits
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col col--6">
-                  <div className={styles.showCard}>
-                    <div className={styles.cardThumbnail}></div>
-                    <div className={styles.cardContent}>
-                      <h4>Wario</h4>
-                      <p>
-                        Clark created a style transfer model to turn your selfie
-                        into Nintendo cartoon
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col col--6">
-                  <div className={styles.showCard}>
-                    <div className={styles.cardCTA}>
-                      Show your PyTorch Live demo here. Send a{' '}
-                      <a
-                        href="https://github.com/pytorch/live/"
-                        target="_blank">
-                        pull request on GitHub
-                      </a>
-                      .
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div className="col col--3">
-              <h2>Join us in our social channels</h2>
-              <p>
+          </div>
+
+          <div className="row">
+            <div className="col col--4">
+              <Card title="Mario and Luigi">
+                Roman finetuned the MobileNetV3 model to identify Nintendo
+                characters.
+              </Card>
+            </div>
+
+            <div className="col col--4">
+              <Card title="Peach">
+                William finetuned the MobileNetV3 model to identify fruits
+              </Card>
+            </div>
+
+            <div className="col col--4">
+              <Card title="Show us what you've made">
+                Submit your PyTorch Live demo to be included here. Send a{' '}
+                <a href="https://github.com/pytorch/live/" target="_blank">
+                  pull request on GitHub.
+                </a>
+              </Card>
+            </div>
+          </div>
+          <div className={clsx(['row', styles.lastRow])}>
+            <div className="col col--4 col--offset-2">
+              <Card
+                thumbnail="social_card1.jpg"
+                title="Join us in our social channels">
                 Engage in conversations and share what you’ve made. Add{' '}
                 <a
                   href="https://twitter.com/search?q=%23pytorchlive"
@@ -103,9 +111,11 @@ function Community() {
                 </a>{' '}
                 hashtag on Twitter, and join our <a href="#">Discord</a>{' '}
                 channels.
-              </p>
-              <h2>Find us on GitHub</h2>
-              <p>
+              </Card>
+            </div>
+
+            <div className="col col--4">
+              <Card thumbnail="social_card2.jpg" title="Find us on Github">
                 File issues, send pull-requests, and check out the latest
                 features on{' '}
                 <a href="https://github.com/pytorch/live/" target="_blank">
@@ -113,7 +123,7 @@ function Community() {
                 </a>
                 . You can also submit a PR to request your demos to be included
                 in our showcase.
-              </p>
+              </Card>
             </div>
           </div>
         </div>

38d6bea3b7f7833bdf03263aa29436d3d65083fe
Christopher Klaiber <crk@fb.com>
2021-11-24 15:40:58 -0800

    ExampleDiffCodeTabs component
    Summary: This new component is used to show example code, and the diff from the previous example, as a set of tabs the reader can move between to either copy/paste the full file, or see the relevant changes.
    
    Reviewed By: raedle
    
    Differential Revision: D32639368
    
    fbshipit-source-id: 014233aeedacc3f382c797e3e6e0a0148658a4f0

---
+++ website/src/components/ExampleDiffCodeTabs.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+
+export default function ExampleDiffCodeTabs({children}) {
+  if (children.length !== 2) {
+    throw new Error(`ExampleDiffCodeTabs requires exactly two children: the diff and the code`);
+  }
+  return <Tabs
+    defaultValue="diff"
+    values={[
+      {label: 'Changes', value: 'diff'},
+      {label: 'Entire File', value: 'code'},
+    ]}>
+    <TabItem value="diff">{children[0]}</TabItem>
+    <TabItem value="code">{children[1]}</TabItem>
+  </Tabs>;
+}

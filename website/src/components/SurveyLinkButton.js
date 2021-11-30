cc1e5c3ba5a665df0cca6c9a0903a56659bed4fb
Clark Sandholtz <clarksandholtz@fb.com>
2021-11-30 15:55:24 -0800

    Update SurveyLinkButton component to point to GitHub issues
    Summary: Link now points to the new tutorial feedback issue creation page.
    
    Reviewed By: raedle
    
    Differential Revision: D32731174
    
    fbshipit-source-id: 29bd13e4e331c5b53ed0e6cd6ce5d0fb803b83dd

--- website/src/components/SurveyLinkButton.js
+++ website/src/components/SurveyLinkButton.js
@@ -3,16 +3,21 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @format
  */
 
 import React from 'react';
 import styles from './SurveyLinkButton.module.css';
 
 export default function SurveyLinkButton({docTitle}) {
-  const surveyUrl = `https://docs.google.com/forms/d/e/1FAIpQLScsB21xJWM_VANad5GcVkQqKB_BptS77axbunzs7ZkwoE5JUw/viewform?usp=pp_url&entry.1880917601=${docTitle.replace(
-    /\s/g,
-    '+',
-  )}`;
+  /*
+   * TODO: T106994597
+   * We don't use docTitle yet, but if we can figure out a way to seed the GitHub form
+   * It will be nice to have them all ready go to
+   */
+  const surveyUrl =
+    'https://github.com/pytorch/live/issues/new?assignees=&labels=Tutorial+Feedback&template=tutorial_feedback.yml';
   return (
     <a href={surveyUrl} target="_blank">
       <div className={styles.surveyLinkBox}>Share what we can improve!</div>

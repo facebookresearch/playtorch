40645167727edbcadad921fe93d97de55e2d0d28
Roman Rädle <raedle@fb.com>
2021-11-29 17:53:47 -0800

    ESLint website
    Summary: Adds linter support to Docusaurus website
    
    Reviewed By: liuyinglao
    
    Differential Revision: D32680069
    
    fbshipit-source-id: 8555432aafb4889558e6fc6e04e1a2df68f7689a

--- website/static/examples/question-answering/QuestionAnsweringDemo.v5.tsx
+++ website/static/examples/question-answering/QuestionAnsweringDemo.v5.tsx
@@ -8,7 +8,7 @@ const model = require('../../models/bert_qa.ptl');
 
 type QuestionAnsweringResult = {
   answer: string;
-}
+};
 
 export default function QuestionAnsweringDemo() {
   // Get safe area insets to account for notches, etc.
@@ -40,11 +40,30 @@ export default function QuestionAnsweringDemo() {
   }
 
   return (
-    <View style={[styles.container, {marginTop: insets.top, marginBottom: insets.bottom}]}>
-      <TextInput style={[styles.item, styles.input]} placeholder="Text" placeholderTextColor="#CCC" multiline={true} value={text} onChangeText={setText} />
-      <TextInput style={[styles.item, styles.input]} placeholder="Question" placeholderTextColor="#CCC" value={question} onChangeText={setQuestion} />
+    <View
+      style={[
+        styles.container,
+        {marginTop: insets.top, marginBottom: insets.bottom},
+      ]}>
+      <TextInput
+        style={[styles.item, styles.input]}
+        placeholder="Text"
+        placeholderTextColor="#CCC"
+        multiline={true}
+        value={text}
+        onChangeText={setText}
+      />
+      <TextInput
+        style={[styles.item, styles.input]}
+        placeholder="Question"
+        placeholderTextColor="#CCC"
+        value={question}
+        onChangeText={setQuestion}
+      />
       <Button title="Ask" onPress={handleAsk} />
-      <Text style={styles.item}>{isProcessing ? "Looking for the answer" : answer}</Text>
+      <Text style={styles.item}>
+        {isProcessing ? 'Looking for the answer' : answer}
+      </Text>
     </View>
   );
 }
@@ -60,5 +79,5 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     color: '#000',
-  }
+  },
 });

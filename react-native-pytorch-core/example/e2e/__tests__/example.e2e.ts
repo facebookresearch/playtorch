5099a061771db9ac48cfacef2e0d1fc8b1b9ebe8
Yinglao Liu <yinglao@fb.com>
2021-12-17 09:56:50 -0800

    add detox e2e test for complex change
    Summary:
    E2E test is useful for us to
    	1	test through app functioning as expected
    	2	test complex change
    
    This is to prepare for the integration of TurboModule, which is hard to test with unit test / integration test
    
    Tests are written in firstTest.e2e.js. For now it is just testing the app can build successfully on both android and ios
    
    Reviewed By: raedle
    
    Differential Revision: D33112317
    
    fbshipit-source-id: baf4ad71f8f4f8e9bbdf26d515bfd30b5d9ca2e4

---
+++ react-native-pytorch-core/example/e2e/__tests__/example.e2e.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import {by, device, element, expect, waitFor} from 'detox';
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('built success', async () => {
+    await waitFor(element(by.text('Playground')))
+      .toBeVisible()
+      .withTimeout(5000);
+    await expect(element(by.text('Astro Bird'))).toBeVisible();
+  });
+});

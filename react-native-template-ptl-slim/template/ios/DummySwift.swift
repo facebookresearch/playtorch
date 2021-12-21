2f29899681397d8d44dc53bffd61fe8402720008
Clark Sandholtz <clarksandholtz@gmail.com>
2021-12-21 15:08:44 -0800

    Creating slim template for PTL

---
+++ react-native-template-ptl-slim/template/ios/DummySwift.swift
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+
+//Created this file to fix the error Could not find or use auto-linked library 'swiftWebKit'
+//https://github.com/facebookarchive/react-native-fbsdk/issues/755

8cc8dbc6a716097b83ef3e050bcbe1b4cc1f50d2
Roman Rädle <raedle@fb.com>
2022-04-25 12:07:33 -0700

    Load models from GitHub
    Summary:
    The release build fails because of the `wav2vec2.ptl` model being too large:
    
    ```
    > Task :app:bundleReleaseJsAndAssets FAILED
    9 actionable tasks: 6 executed, 3 up-to-date
    warn Package types/jest has been ignored because it contains invalid configuration. Reason: Package subpath './package.json' is not defined by "exports" in /Users/raedle/fbsource/xplat/pytorch/live/react-native-pytorch-core/example/node_modules/types/jest/package.json
    error models/wav2vec2.ptl: Invalid string length
    ```
    
    Instead of bundling the models or loading them via Metro in `__DEV__`, models will be loaded from the `pytorch/live` GitHub repo (releases v0.1.0 assets).
    
    Reviewed By: clarksandholtz
    
    Differential Revision: D35362938
    
    fbshipit-source-id: 3ecb800ffb1239de7a469c7142b4e9f59453fdc7

--- react-native-pytorch-core/example/src/Models.ts
+++ react-native-pytorch-core/example/src/Models.ts
@@ -9,47 +9,51 @@
 
 import type {ModelInfo} from 'react-native-pytorch-core';
 
-// Example for how to export a model with the live.spec.json bundled as extra files
-// Google Colab: https://colab.research.google.com/drive/1vGlRddixmdJvyJ0kTl3uYXuXZ8Q2Ir4C
 export const ImageClassificationModels: ModelInfo[] = [
   {
     name: 'ResNet 18',
-    model: require('../models/resnet18.ptl'),
+    model:
+      'https://github.com/pytorch/live/releases/download/v0.1.0/resnet18.ptl',
   },
   {
     name: 'MobileNet V3 Small',
-    model: require('../models/mobilenet_v3_small.ptl'),
+    model:
+      'https://github.com/pytorch/live/releases/download/v0.1.0/mobilenet_v3_small.ptl',
   },
   {
     name: 'MobileNet V3 Large',
-    model: require('../models/mobilenet_v3_large.ptl'),
+    model:
+      'https://github.com/pytorch/live/releases/download/v0.1.0/mobilenet_v3_large.ptl',
   },
 ];
 
 export const MultiClassClassificationModels: ModelInfo[] = [
   {
     name: 'MNIST',
-    model: require('../models/mnist.ptl'),
+    model: 'https://github.com/pytorch/live/releases/download/v0.1.0/mnist.ptl',
   },
 ];
 
 export const ObjectDetectionModels: ModelInfo[] = [
   {
     name: 'DETR',
-    model: require('../models/detr_resnet50.ptl'),
+    model:
+      'https://github.com/pytorch/live/releases/download/v0.1.0/detr_resnet50.ptl',
   },
 ];
 
 export const NLPModels: ModelInfo[] = [
   {
     name: 'DistilBertQA',
-    model: require('../models/bert_qa.ptl'),
+    model:
+      'https://github.com/pytorch/live/releases/download/v0.1.0/bert_qa.ptl',
   },
 ];
 
 export const AudioModels: ModelInfo[] = [
   {
     name: 'Wav2Vec2',
-    model: require('../models/wav2vec2.ptl'),
+    model:
+      'https://github.com/pytorch/live/releases/download/v0.1.0/wav2vec2.ptl',
   },
 ];

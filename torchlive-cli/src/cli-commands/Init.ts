525732d3685fecd1ff1f7358605deac550a01ca9
Roman Rädle <raedle@fb.com>
2022-06-15 19:58:13 -0700

    Remove template post init script error check
    Summary: The `init` command included a check if the template post init script failed. This post init script was only needed to export models. The model export has been removed, which makes this check and code obsolete. This change removes the code
    
    Reviewed By: chrisklaiber
    
    Differential Revision: D37185829
    
    fbshipit-source-id: ab1d471f2899adb70ade96bd5ebabbcb1f31a15b

--- torchlive-cli/src/cli-commands/Init.ts
+++ torchlive-cli/src/cli-commands/Init.ts
@@ -15,7 +15,6 @@ import {getEnv} from '../utils/SystemUtils';
 import {executeCommandForTask, runTasks} from '../utils/TaskUtils';
 import {isCommandInstalled} from '../utils/ToolingUtils';
 import {ListrGetRendererTaskOptions, ListrDefaultRenderer} from 'listr2';
-import fs from 'fs';
 import chalk from 'chalk';
 
 const NOBREAKSPACE_INTEDENTATION = '‎ ‎ ‎ ‎ ';
@@ -124,10 +123,6 @@ ${NOBREAKSPACE_INTEDENTATION}`);
   }
 
   getPostInitMessage(): string {
-    let errorMessage = this.getPostInitScriptError();
-    if (errorMessage !== null) {
-      return errorMessage;
-    }
     return `Initialized ${this.getDescription()}
     ${NOBREAKSPACE_INTEDENTATION}
     ${chalk.green('Run instructions for Android')}:
@@ -144,32 +139,6 @@ ${NOBREAKSPACE_INTEDENTATION}`);
     }.xcworkspace in Xcode or run "xed -b ios"
     ${NOBREAKSPACE_INTEDENTATION}• Hit the Run button
     ${NOBREAKSPACE_INTEDENTATION}`;
-  }
-
-  getPostInitScriptError(): string | null {
-    // check if the error.log exists
-    const errorLogPath = `${this.name}/models/error.log`;
-    const issuePageUrl = 'https://github.com/pytorch/live/issues';
-    try {
-      if (fs.existsSync(`${this.name}/models/error.log`)) {
-        const errorMessage = fs.readFileSync(errorLogPath).toString();
-        const prefix = `${chalk.redBright('Error')} project "${
-          this.name
-        }" is created but we ran into the following issues at generating example models: \n${NOBREAKSPACE_INTEDENTATION}\n`;
-        const suffix = `\n${NOBREAKSPACE_INTEDENTATION}\nplease rerun
-${NOBREAKSPACE_INTEDENTATION}• "python make_models.py" in the ${this.name}/models/ directory, and\n
-${NOBREAKSPACE_INTEDENTATION}• "pod install" in the ${this.name}/ios/ directory \n
-if the issue persists, please report at ${issuePageUrl}.`;
-        return errorMessage ? prefix + errorMessage + suffix : null;
-      }
-    } catch (err) {
-      return `${chalk.redBright(
-        'Error',
-      )} The torchlive-cli init command ran into unexpected error at validating the models generated under ${
-        this.name
-      }/models/, please report the issue at ${issuePageUrl}.`;
-    }
-    return null;
   }
 }
 

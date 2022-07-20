15d44b2aecf8d32314faccfd9dd5ced07ad2eac5
Roman Rädle <raedle@fb.com>
2022-07-19 21:18:04 -0700

    Redirect components
    Summary:
    Replace markdown redirect pages with React redirect components, which will not show the page chrome (i.e., navigation etc.)
    
    Links:
    
    * http://localhost:3000/app/privacy/policy
    * http://localhost:3000/app/legal/terms
    
    Reviewed By: anoopsinha
    
    Differential Revision: D37983399
    
    fbshipit-source-id: da52469beaba61969351e48ea6cce185b5b18b60

--- website/src/pages/app/privacy/policy.md
+++
@@ -1,4 +0,0 @@
-import ExternalRedirect from '@site/src/components/ExternalRedirect';
-import ExternalLinks from '@site/src/constants/ExternalLinks';
-
-<ExternalRedirect to={ExternalLinks.PRIVACY_POLICY} />
---
+++ website/src/pages/app/privacy/policy/index.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import * as React from 'react';
+import ExternalRedirect from '@site/src/components/ExternalRedirect';
+import ExternalLinks from '@site/src/constants/ExternalLinks';
+
+export default function PrivacyPolicy() {
+  return <ExternalRedirect to={ExternalLinks.PRIVACY_POLICY} />;
+}

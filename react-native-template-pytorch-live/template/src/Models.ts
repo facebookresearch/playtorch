e37fc71eabcdfe26b320efcc56c0ca73cd350140
Roman Rädle <raedle@fb.com>
2022-09-21 13:02:26 -0700

    Update model urls
    Summary: Update model urls from previous GitHub repo `pytorch/live` to `facebookresearch/playtorch`
    
    Reviewed By: justinhaaheim
    
    Differential Revision: D39680692
    
    fbshipit-source-id: e5ed43e9fad4cfcb4275b7bace7cf8ce994859f8

--- react-native-template-pytorch-live/template/src/Models.ts
+++ react-native-template-pytorch-live/template/src/Models.ts
@@ -13,24 +13,25 @@ export const ImageClassificationModels: ModelInfo[] = [
   {
     name: 'ResNet 18',
     model:
-      'https://github.com/pytorch/live/releases/download/v0.1.0/resnet18.ptl',
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/resnet18.ptl',
   },
   {
     name: 'MobileNet V3 Small',
     model:
-      'https://github.com/pytorch/live/releases/download/v0.1.0/mobilenet_v3_small.ptl',
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/mobilenet_v3_small.ptl',
   },
   {
     name: 'MobileNet V3 Large',
     model:
-      'https://github.com/pytorch/live/releases/download/v0.1.0/mobilenet_v3_large.ptl',
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/mobilenet_v3_large.ptl',
   },
 ];
 
 export const MultiClassClassificationModels: ModelInfo[] = [
   {
     name: 'MNIST',
-    model: 'https://github.com/pytorch/live/releases/download/v0.1.0/mnist.ptl',
+    model:
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/mnist.ptl',
   },
 ];
 
@@ -38,7 +39,7 @@ export const ObjectDetectionModels: ModelInfo[] = [
   {
     name: 'DETR',
     model:
-      'https://github.com/pytorch/live/releases/download/v0.1.0/detr_resnet50.ptl',
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/detr_resnet50.ptl',
   },
 ];
 
@@ -46,8 +47,8 @@ export const NLPModels: ModelInfo[] = [
   {
     name: 'DistilBertQA',
     model:
-      'https://github.com/pytorch/live/releases/download/v0.1.0/bert_qa.ptl',
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/bert_qa.ptl',
     vocab:
-      'https://github.com/pytorch/live/releases/download/v0.1.0/BERTVocab.json',
+      'https://github.com/facebookresearch/playtorch/releases/download/v0.1.0/BERTVocab.json',
   },
 ];

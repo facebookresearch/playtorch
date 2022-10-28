a0c99052b93d50991d9fe0c98aad8b2086287b89
zrfisher <zrfisher@fb.com>
2022-10-28 15:13:42 -0700

    Add function to generate function implementation for all possible signatures of one op
    Differential Revision:
    D40231867
    
    Commit 2https://internalfb.com/D40231863
    
    fbshipit-source-id: ac2485b7f3efb5915fdee1081d70a9a3e5ef6abd

--- react-native-pytorch-core/cxx/codegen/test.py
+++ react-native-pytorch-core/cxx/codegen/test.py
@@ -8,10 +8,13 @@ from unittest import TestCase
 from ..codegen.mock_dict import ops_decl
 
 from ..codegen.op_data_structures import OpGroup, OpInfo
-from ..codegen.playtorch_codegen import gen_cpp_func
+from ..codegen.playtorch_codegen import gen_cpp_func, gen_cpp_func_impl
 from ..codegen.test_code_strings import (
     gen_cpp_func_add0,
     gen_cpp_func_add1,
+    gen_cpp_func_impl_add,
+    gen_cpp_func_impl_item,
+    gen_cpp_func_impl_reshape,
     gen_cpp_func_item,
     gen_cpp_func_reshape,
 )
@@ -82,3 +85,24 @@ class DataStructuresTest(TestCase):
         self.assertEqual(ops_dict["add"].ops[1].implemented, True)
         self.assertEqual(ops_dict["reshape"].ops[0].implemented, False)
         self.assertEqual(ops_dict["item"].ops[0].implemented, True)
+
+    def test_gen_cpp_func_impl(self):
+        ops_dict = {}
+        # add is fully implemented, reshape has argument not implemented
+        # no example of all arguments implemented but return type implemented exists playtorch v0.2.2
+        tensor_ops = ["add", "reshape", "item"]
+        for op in ops_decl:
+            if "Tensor" in op["method_of"] and op["operator_name"] in tensor_ops:
+                op = OpInfo.from_dict(op)
+                if op.name in ops_dict:
+                    ops_dict[op.name].ops.append(op)
+                else:
+                    ops_dict[op.name] = OpGroup(op)
+        self.assertEqual(gen_cpp_func_impl(ops_dict["add"]), gen_cpp_func_impl_add)
+        self.assertEqual(
+            gen_cpp_func_impl(ops_dict["reshape"]), gen_cpp_func_impl_reshape
+        )
+        self.assertEqual(gen_cpp_func_impl(ops_dict["item"]), gen_cpp_func_impl_item)
+        self.assertEqual(ops_dict["add"].implemented, True)
+        self.assertEqual(ops_dict["reshape"].implemented, False)
+        self.assertEqual(ops_dict["item"].implemented, True)

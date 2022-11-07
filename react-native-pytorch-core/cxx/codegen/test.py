4153ee21dc6ef779d65447df7c02ca15b068aeda
zrfisher <zrfisher@fb.com>
2022-11-07 07:47:31 -0800

    Refactor
    Differential Revision: D41082853
    
    fbshipit-source-id: 9a18645b23b532356d459e40adc31e6779c8f639

--- react-native-pytorch-core/cxx/codegen/test.py
+++ react-native-pytorch-core/cxx/codegen/test.py
@@ -6,18 +6,7 @@
 from unittest import TestCase
 
 from ..codegen.mock_dict import ops_decl
-from ..codegen.op_data_structures import OpGroup, OpInfo
-from ..codegen.playtorch_codegen import gen_cpp_file, gen_cpp_func, gen_cpp_func_impl
-from ..codegen.test_code_strings import (
-    cpp_file_str,
-    gen_cpp_func_add0,
-    gen_cpp_func_add1,
-    gen_cpp_func_impl_add,
-    gen_cpp_func_impl_item,
-    gen_cpp_func_impl_reshape,
-    gen_cpp_func_item,
-    gen_cpp_func_reshape,
-)
+from ..codegen.op_data_structures import camelCase, OpGroup, OpInfo
 
 
 class DataStructuresTest(TestCase):
@@ -27,8 +16,8 @@ class DataStructuresTest(TestCase):
         self.assertEqual(
             op_info.schema_order_cpp_signature, "at::Tensor (const at::Tensor &, bool)"
         )
-        self.assertEqual(op_info.returns_name, "result")
-        self.assertEqual(op_info.returns_type, "at::Tensor")
+        self.assertEqual(op_info.returns[0].name, "result")
+        self.assertEqual(op_info.returns[0].type_, "at::Tensor")
 
     def test_argument(self):
         op_info = OpInfo.from_dict(ops_decl[0])
@@ -65,56 +54,21 @@ class DataStructuresTest(TestCase):
         self.assertEqual(len(ops_dict["item"].ops), 1)
         self.assertEqual(ops_dict["item"].min_num_required, 0)
 
-    def test_gen_cpp_func(self):
+    def test_camel_case(self):
         ops_dict = {}
-        # add is fully implemented, reshape has argument not implemented, item takes no arguments
-        # no example of all arguments implemented but return type implemented exists playtorch v0.2.2
-        tensor_ops = ["add", "reshape", "item"]
+        tensor_ops = ["add", "div"]
         for op in ops_decl:
             if "Tensor" in op["method_of"] and op["operator_name"] in tensor_ops:
                 op = OpInfo.from_dict(op)
-                if op.name in ops_dict:
-                    ops_dict[op.name].ops.append(op)
+                nameCamelCase = camelCase(op.name)
+                if nameCamelCase in ops_dict:
+                    ops_dict[nameCamelCase].ops.append(op)
                 else:
-                    ops_dict[op.name] = OpGroup(op)
-        self.assertEqual(gen_cpp_func(ops_dict["add"].ops[0]), gen_cpp_func_add0)
-        self.assertEqual(gen_cpp_func(ops_dict["add"].ops[1]), gen_cpp_func_add1)
-        self.assertEqual(gen_cpp_func(ops_dict["reshape"].ops[0]), gen_cpp_func_reshape)
-        self.assertEqual(gen_cpp_func(ops_dict["item"].ops[0]), gen_cpp_func_item)
-        self.assertEqual(ops_dict["add"].ops[0].implemented, True)
-        self.assertEqual(ops_dict["add"].ops[1].implemented, True)
-        self.assertEqual(ops_dict["reshape"].ops[0].implemented, False)
-        self.assertEqual(ops_dict["item"].ops[0].implemented, True)
-
-    def test_gen_cpp_func_impl(self):
-        ops_dict = {}
-        # add is fully implemented, reshape has argument not implemented
-        # no example of all arguments implemented but return type implemented exists playtorch v0.2.2
-        tensor_ops = ["add", "reshape", "item"]
-        for op in ops_decl:
-            if "Tensor" in op["method_of"] and op["operator_name"] in tensor_ops:
-                op = OpInfo.from_dict(op)
-                if op.name in ops_dict:
-                    ops_dict[op.name].ops.append(op)
-                else:
-                    ops_dict[op.name] = OpGroup(op)
-        self.assertEqual(gen_cpp_func_impl(ops_dict["add"]), gen_cpp_func_impl_add)
-        self.assertEqual(
-            gen_cpp_func_impl(ops_dict["reshape"]), gen_cpp_func_impl_reshape
-        )
-        self.assertEqual(gen_cpp_func_impl(ops_dict["item"]), gen_cpp_func_impl_item)
-        self.assertEqual(ops_dict["add"].implemented, True)
-        self.assertEqual(ops_dict["reshape"].implemented, False)
-        self.assertEqual(ops_dict["item"].implemented, True)
-
-    def test_gen_cpp_file(self):
-        ops_dict = {}
-        tensor_ops = ["add", "sub", "mul", "reshape", "item"]
-        for op in ops_decl:
-            if "Tensor" in op["method_of"] and op["operator_name"] in tensor_ops:
-                op = OpInfo.from_dict(op)
-                if op.name in ops_dict:
-                    ops_dict[op.name].ops.append(op)
-                else:
-                    ops_dict[op.name] = OpGroup(op)
-        self.assertEqual(gen_cpp_file(ops_dict), cpp_file_str)
+                    ops_dict[nameCamelCase] = OpGroup(op)
+        self.assertEqual(ops_dict["add"].ops[0].arguments[0].name, "self")
+        self.assertEqual(ops_dict["div"].ops[0].arguments[2].name, "roundingMode")
+        self.assertEqual(camelCase("_add_relu"), "_addRelu")
+        self.assertEqual(camelCase("_add_relu_"), "_addRelu_")
+        self.assertEqual(camelCase("add_relu_"), "addRelu_")
+        self.assertEqual(camelCase("add_relu"), "addRelu")
+        self.assertEqual(camelCase("add"), "add")

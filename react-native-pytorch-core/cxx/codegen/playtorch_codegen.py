161adcdb3c99a9301507f7bd138a940097238a71
zrfisher <zrfisher@fb.com>
2022-11-07 07:42:34 -0800

    Add function to generate function implementation for all possible signatures of one op
    Differential Revision:
    D40231867
    
    Commit 2https://internalfb.com/D40231863
    
    fbshipit-source-id: 611c5dfa72352fe228be2a64f855a89d5eb7b9e2

--- react-native-pytorch-core/cxx/codegen/playtorch_codegen.py
+++ react-native-pytorch-core/cxx/codegen/playtorch_codegen.py
@@ -4,12 +4,18 @@
 # LICENSE file in the root directory of this source tree.
 
 from ..codegen.code_strings import (
+    cpp_function_implementation_end,
+    cpp_function_implementation_start,
+    cpp_throw_BigInt,
+    cpp_throw_error_template,
     cpp_try_signature_template,
     get_argument_string,
     get_check_argument_types_string,
     get_returns_string,
 )
-from ..codegen.op_data_structures import OpInfo
+from ..codegen.op_data_structures import OpGroup, OpInfo
+
+bigint_error_ops = ["item"]
 
 
 def gen_cpp_func(op: OpInfo):
@@ -33,3 +39,30 @@ def gen_cpp_func(op: OpInfo):
         }
     )
     return code_string
+
+
+def gen_cpp_func_impl(op_group: OpGroup) -> str:
+    function_string = cpp_function_implementation_start.substitute(
+        {"operator_name": op_group.name, "num_required_args": op_group.min_num_required}
+    )
+    if op_group.name in bigint_error_ops:
+        function_string += cpp_throw_BigInt.substitute({"op_name": op_group.name})
+    op_group.ops.sort(key=lambda op: op.num_required, reverse=True)
+    for op in op_group.ops:
+        function_string += gen_cpp_func(op)
+    signatures_string = ", ".join(
+        [op.schema_order_cpp_signature for op in op_group.ops]
+    )
+    function_string += cpp_throw_error_template.substitute(
+        {"op_name": op.name, "signatures": signatures_string}
+    )
+    function_string += "\n"
+    function_string += cpp_function_implementation_end
+    function_string += "\n"
+    all_ops_implemented = True
+    for op in op_group.ops:
+        if not op.implemented:
+            all_ops_implemented = False
+            break
+    op_group.implemented = all_ops_implemented
+    return function_string

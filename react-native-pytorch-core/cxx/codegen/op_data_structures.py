37cf5ba917c1c1d28f7af5680acbc8b92803f642
zrfisher <zrfisher@fb.com>
2022-11-07 07:27:20 -0800

    Refactor
    Differential Revision: D41082853
    
    fbshipit-source-id: 7ba70c4908c23797edc1a1f33b2d7b40673a0d59

--- react-native-pytorch-core/cxx/codegen/op_data_structures.py
+++ react-native-pytorch-core/cxx/codegen/op_data_structures.py
@@ -6,6 +6,24 @@
 from typing import Optional
 
 
+def camelCase(snake_case: str) -> str:
+    prefix = "_" if snake_case[0] == "_" else ""
+    suffix = "_" if snake_case[-1] == "_" else ""
+    words = snake_case.split("_")
+    if words[0] == "":
+        words.pop(0)
+    return prefix + words[0] + "".join([word.title() for word in words[1:]]) + suffix
+
+
+class Return:
+    name: str
+    type_: str
+
+    def __init__(self, name: str, type_: str):
+        self.name = name
+        self.type_ = type_
+
+
 class Argument:
     annotation: Optional[str]
     default: Optional[any]
@@ -22,9 +40,15 @@ class Argument:
         self.kwarg_only = argInfo["kwarg_only"] if "kwarg_only" in argInfo else False
         self.dynamic_type = argInfo["dynamic_type"]
         self.is_nullable = argInfo["is_nullable"] if "is_nullable" in argInfo else False
-        self.name = argInfo["name"]
+        self.name = camelCase(argInfo["name"])
         self.type_ = argInfo["type"]
         self.implemented = False
+
+    def is_kwarg(self) -> bool:
+        return self.default is not None or self.kwarg_only
+
+    def is_required(self) -> bool:
+        return self.default is None
 
 
 class OpInfo:
@@ -32,8 +56,8 @@ class OpInfo:
     schema_order_cpp_signature: str
     arguments: [Argument]
     num_required: int
-    returns_name: str
-    returns_type: str
+    options_index: int
+    returns: [Return]
     arg_types: [str]
     implemented: bool  # not from Declarations, initally false but will be set to true if a case for all argument types in get_arguments_string and return type in get_return_type exists
 
@@ -42,8 +66,7 @@ class OpInfo:
         name: str,
         schema_order_cpp_signature: str,
         arguments: [Argument],
-        returns_name: str,
-        returns_type: str,
+        returns: [Return],
     ):
         self.name = name
         self.schema_order_cpp_signature = schema_order_cpp_signature
@@ -51,8 +74,13 @@ class OpInfo:
         self.num_required = sum(
             [1 if arg.default is None else 0 for arg in arguments[1:]]
         )
-        self.returns_name = returns_name
-        self.returns_type = returns_type
+        self.options_index = self.num_required - sum(
+            [
+                1 if arg.kwarg_only and arg.default is None else 0
+                for arg in arguments[1:]
+            ]
+        )
+        self.returns = returns
         self.arg_types = [arg.type_ for arg in arguments]
         self.implemented = False
 
@@ -61,11 +89,8 @@ class OpInfo:
         name = op["name"]
         schema_order_cpp_signature = op["schema_order_cpp_signature"]
         arguments = [Argument(arg) for arg in op["schema_order_arguments"]]
-        returns_name = op["returns"][0]["name"]
-        returns_type = op["returns"][0]["type"]
-        return OpInfo(
-            name, schema_order_cpp_signature, arguments, returns_name, returns_type
-        )
+        returns = [Return(r["name"], r["type"]) for r in op["returns"]]
+        return OpInfo(name, schema_order_cpp_signature, arguments, returns)
 
 
 class OpGroup:
@@ -75,7 +100,7 @@ class OpGroup:
     implemented: bool  # initially False, will be set to true if OpInfo.implemented for every op in ops
 
     def __init__(self, op: OpInfo):
-        self.name = op.name
+        self.name = camelCase(op.name)
         self.ops = [op]
         self.min_num_required = op.num_required
         self.implemented = False
